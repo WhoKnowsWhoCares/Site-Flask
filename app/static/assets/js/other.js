7c52bca6616a0bd7b7beea0c3ec64073f8356efe
asFrants <as.fratnsev@gmail.com>
2023-06-16 14:22:41 +0300

    update

--- app/static/assets/js/other.js
+++ app/static/assets/js/other.js
@@ -1,16 +1,21 @@
-var img = document.querySelector('.img');
-var fullScreen = document.querySelector('.fullscreen');
-var close = document.querySelector('.close');
-var zoomedImg = document.querySelector('.fullscreen img');
-
-img.addEventListener('click', function() {
-    fullScreen.style.display = 'flex';
-});
-
-close.addEventListener('click', function() {
-    fullScreen.style.display = 'none';
-});
-
-zoomedImg.addEventListener('click', function() {
-    fullScreen.style.display = 'none';
-});
+window.onload = () => {
+    // (A) GET ALL IMAGES
+    let all = document.getElementsByClassName("zoomable");
+   
+    // (B) CLICK TO GO FULLSCREEN
+    if (all.length>0) { for (let i of all) {
+      i.onclick = () => {
+        // (B1) EXIT FULLSCREEN
+        if (document.fullscreenElement != null || document.webkitFullscreenElement != null) {
+          if (document.exitFullscreen) { document.exitFullscreen(); }
+          else { document.webkitCancelFullScreen(); }
+        }
+   
+        // (B2) ENTER FULLSCREEN
+        else {
+          if (i.requestFullscreen) { i.requestFullscreen(); }
+          else { i.webkitRequestFullScreen(); }
+        }
+      };
+    }}
+  };
