051d52af8f890f2650aec971b1ef6a00816184d7
asFrants <as.fratnsev@gmail.com>
2023-09-26 14:53:14 +0300

    change tests a bit

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,7 +1,7 @@
 import unittest
 import time
 from app import create_app, db
-from app.models import User, AnonymousUser, Role, Permission
+from app.models import User, AnonymousUser, UserRole
 
 
 class UserModelTestCase(unittest.TestCase):
@@ -10,7 +10,6 @@ class UserModelTestCase(unittest.TestCase):
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
-        Role.insert_roles()
 
     def tearDown(self):
         db.session.remove()
@@ -18,49 +17,32 @@ class UserModelTestCase(unittest.TestCase):
         self.app_context.pop()
 
     def test_password_setter(self):
-        u = User(password='cat')
+        u = User(username='john', email='john@example.com', password='cat')
         self.assertTrue(u.password_hash is not None)
 
     def test_no_password_getter(self):
-        u = User(password='cat')
+        u = User(username='john', email='john@example.com', password='cat')
         with self.assertRaises(AttributeError):
             u.password
 
     def test_password_salts_are_random(self):
-        u = User(password='cat')
-        u2 = User(password='cat')
+        u = User(username='john', email='john@example.com', password='cat')
+        u2 = User(username='anna', email='anna@example.com', password='cat')
         self.assertTrue(u.password_hash != u2.password_hash)
 
     def test_user_role(self):
-        u = User(email='john@example.com', password='cat')
-        self.assertTrue(u.can(Permission.FOLLOW))
-        self.assertTrue(u.can(Permission.COMMENT))
-        self.assertTrue(u.can(Permission.WRITE))
-        self.assertFalse(u.can(Permission.MODERATE))
-        self.assertFalse(u.can(Permission.ADMIN))
-
-    def test_moderator_role(self):
-        r = Role.query.filter_by(name='Moderator').first()
-        u = User(email='john@example.com', password='cat', role=r)
-        self.assertTrue(u.can(Permission.FOLLOW))
-        self.assertTrue(u.can(Permission.COMMENT))
-        self.assertTrue(u.can(Permission.WRITE))
-        self.assertTrue(u.can(Permission.MODERATE))
-        self.assertFalse(u.can(Permission.ADMIN))
+        u = User(username='john', email='john@example.com', password='cat', role=UserRole.USER)
+        self.assertTrue(u.can(UserRole.USER))
+        self.assertFalse(u.can(UserRole.ADMIN))
+        self.assertFalse(u.is_administrator())
 
     def test_administrator_role(self):
-        r = Role.query.filter_by(name='Administrator').first()
-        u = User(email='john@example.com', password='cat', role=r)
-        self.assertTrue(u.can(Permission.FOLLOW))
-        self.assertTrue(u.can(Permission.COMMENT))
-        self.assertTrue(u.can(Permission.WRITE))
-        self.assertTrue(u.can(Permission.MODERATE))
-        self.assertTrue(u.can(Permission.ADMIN))
+        u = User(username='john', email='john@example.com', password='cat', role=UserRole.ADMIN)
+        self.assertFalse(u.can(UserRole.USER))
+        self.assertTrue(u.can(UserRole.ADMIN))
+        self.assertTrue(u.is_administrator())
 
     def test_anonymous_user(self):
         u = AnonymousUser()
-        self.assertFalse(u.can(Permission.FOLLOW))
-        self.assertFalse(u.can(Permission.COMMENT))
-        self.assertFalse(u.can(Permission.WRITE))
-        self.assertFalse(u.can(Permission.MODERATE))
-        self.assertFalse(u.can(Permission.ADMIN))
+        self.assertFalse(u.can(UserRole.USER))
+        self.assertFalse(u.can(UserRole.ADMIN))
