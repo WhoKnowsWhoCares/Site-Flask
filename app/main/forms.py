e10c4bcc8c7a8485321eb093fb175591920394fc
Alexander <as.frantsev@gmail.com>
2024-02-29 10:24:02 +0300

    update errors page

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,9 +4,7 @@ from wtforms.validators import DataRequired, Length, Email
 
 
 class MessageForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired(), Length(1, 64)])
-    email = StringField('email', validators=[DataRequired(), Length(1, 64), Email()])
-    message = StringField('message', validators=[DataRequired(), Length(1, 64)])
-    submit = SubmitField('submit')
-
-
+    name = StringField("name", validators=[DataRequired(), Length(1, 64)])
+    email = StringField("email", validators=[DataRequired(), Length(1, 64), Email()])
+    message = StringField("message", validators=[DataRequired(), Length(1, 64)])
+    submit = SubmitField("submit")
