8f1126ce872d74a280c24299fa8b88d649405cf9
Alexander <as.frantsev@gmail.com>
2024-09-29 18:17:48 +0300

    change mount point

--- config.py
+++ config.py
@@ -71,7 +71,7 @@ class ProdPgConfig(Config):
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASS")
     DB_HOST = os.getenv("DB_HOST")
-    # DB_PORT = os.getenv("DB_PORT")
+    DB_PORT = os.getenv("DB_PORT")
     DB_NAME = os.getenv("DB_NAME")
     SQLALCHEMY_DATABASE_URI = "postgresql+pg8000://{}:{}@{}/{}".format(
         DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
